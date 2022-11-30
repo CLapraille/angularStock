e608751b697b7498f476cba58931f28b871e642a
CLapraille <116072182+CLapraille@users.noreply.github.com>
2022-11-30 09:18:42 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewStockComponent } from './stock-tracker/components/new-stock/new-stock.component';
+import { StockListComponent } from './stock-tracker/components/stock-list/stock-list.component';
+import { StockDetailComponent } from './stock-tracker/components/stock-detail/stock-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewStockComponent,
+    StockListComponent,
+    StockDetailComponent
   ],
   imports: [
     BrowserModule
